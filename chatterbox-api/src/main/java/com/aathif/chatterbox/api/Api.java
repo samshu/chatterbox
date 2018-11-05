d7b78296b8f687a14467ec58ba1246d5803860e4
Mohammed Aathif <mohammed.aathif@nokia.com>
2018-11-05 19:44:31 +0530

    Service based interactions between bundles

--- chatterbox-api/src/main/java/com/aathif/chatterbox/api/Api.java
+++ chatterbox-api/src/main/java/com/aathif/chatterbox/api/Api.java
@@ -2,14 +2,18 @@ package com.aathif.chatterbox.api;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
 
-import com.aathif.chatterbox.services.UserServiceImpl;
+import com.aathif.chatterbox.services.UserService;
 
 public class Api implements BundleActivator {
 
+	private ServiceReference<UserService> userService;
+
 	public void start(BundleContext context) throws Exception {
 		System.out.println("Started chatterbox-api");
-		UserAPI userAPI = new UserAPI(new UserServiceImpl());
+		userService = context.getServiceReference(UserService.class);
+		UserAPI userAPI = new UserAPI(context.getService(userService));
 		userAPI.getAllUsers();
 	}
 
--- chatterbox-services/src/main/java/com/aathif/chatterbox/App.java
+++ chatterbox-services/src/main/java/com/aathif/chatterbox/App.java
@@ -1,15 +1,25 @@
 package com.aathif.chatterbox;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+
+import com.aathif.chatterbox.services.UserService;
+import com.aathif.chatterbox.services.UserServiceImpl;
 
 public class App implements BundleActivator {
 
-	public void start(BundleContext arg0) throws Exception {
-		System.out.println("App Started Successfully");
+	private ServiceRegistration userService;
+	
+	public void start(BundleContext bundleCtx) throws Exception {
+		System.out.println("Starting chatterbox-services...");
+		userService = bundleCtx.registerService(UserService.class.getName(), new UserServiceImpl(), null);
+		System.out.println("chatterbox-services Started Successfully");
 	}
 
-	public void stop(BundleContext arg0) throws Exception {
-		System.out.println("App Stopped Succesfully");
+	public void stop(BundleContext bundleCtx) throws Exception {
+		System.out.println("Stopping chatterbox-services...");
+		userService.unregister();
+		System.out.println("chatterbox-services Stopped Succesfully");
 	}
 
 }
